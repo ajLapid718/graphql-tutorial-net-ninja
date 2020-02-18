c05d41ef7d827eb6b0980cd8744787b3495513fe
ajLapid718 <ajLapid718@gmail.com>
2020-02-18 00:19:48 -0500

    mount graphql middleware and prepare to pass in schema

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+const graphqlHTTP = require("express-graphql");
+
+// graphqlHTTP is a function, which takes in an options object, that will fire off on every incoming request;
+// middleware for express; takes an options object or function as input to configure behavior, and returns an express middleware;
+
+// if you navigate to this endpoint, at this point in time, this will be the output on the client;
+
+/*
+
+{
+"errors": [
+    {
+      "message": "GraphQL middleware options must contain a schema."
+    }
+  ]
+}
+
+*/
+
+app.use("/graphql", graphqlHTTP({
+
+}));
+
 app.listen(PORT, () => {
   console.log(`now listening for requests on port: ${PORT}`)
 });
