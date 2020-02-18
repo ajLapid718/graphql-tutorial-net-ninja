05a0ed279b35a03eb93567de063c6d57d7b99534
ajLapid718 <ajLapid718@gmail.com>
2020-02-18 01:06:35 -0500

    import schema and pass it to the options object in middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 4000;
 
 const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema.js");
 
 // graphqlHTTP is a function, which takes in an options object, that will fire off on every incoming request;
 // middleware for express; takes an options object or function as input to configure behavior, and returns an express middleware;
@@ -22,7 +23,7 @@ const graphqlHTTP = require("express-graphql");
 */
 
 app.use("/graphql", graphqlHTTP({
-
+  schema // schema: schema;
 }));
 
 app.listen(PORT, () => {
