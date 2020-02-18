e01e2427e4e43ac0c0215b17c1cf97cf25a260c5
ajLapid718 <ajLapid718@gmail.com>
2020-02-18 00:39:22 -0500

    describe the shape of the Book object

--- schema/schema.js
+++ schema/schema.js
@@ -3,3 +3,21 @@
 // our schema will define the data on the graph --- object types, relationships, instructions on how to query/retrieve/mutate data;
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLString } = graphql;
+
+// schema definition: by defining a schema we define the entities that exist, but also the different queries and mutations that are possible to make;
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => {
+    return {
+      id: { 
+        type: GraphQLString 
+      },
+      name: {
+        type: GraphQLString
+      },
+      genre: {
+        type: GraphQLString
+      }
+    }
+  }
+})
