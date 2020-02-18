4a999a3f44e90cd9d46c335d650d900c7babf66c
ajLapid718 <ajLapid718@gmail.com>
2020-02-18 00:31:32 -0500

    destructure type checking functions native to graphql module

--- schema/schema.js
+++ schema/schema.js
@@ -2,3 +2,4 @@
 // define schema here;
 // our schema will define the data on the graph --- object types, relationships, instructions on how to query/retrieve/mutate data;
 const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString } = graphql;
