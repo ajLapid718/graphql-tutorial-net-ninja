2586d50c04798888c6da178fd3c7cf228d0399db
ajLapid718 <ajLapid718@gmail.com>
2020-02-18 00:29:55 -0500

    declare purpose of schema file

--- schema/schema.js
+++ schema/schema.js
@@ -1,1 +1,4 @@
 // prepare to set up schema for/with GraphQL;
+// define schema here;
+// our schema will define the data on the graph --- object types, relationships, instructions on how to query/retrieve/mutate data;
+const graphql = require("graphql");
