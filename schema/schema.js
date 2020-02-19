23d92fdfee898bd1a133f20f0cce5381a07463da
ajLapid718 <ajLapid718@gmail.com>
2020-02-18 23:49:00 -0500

    clean up comments and temporary edits to field names

--- schema/schema.js
+++ schema/schema.js
@@ -81,8 +81,8 @@ const AuthorType = new GraphQLObjectType({
 
 // responsibility 2: define relationships (if necessary) and responsibility 3: define root query (queries);
 const RootQuery = new GraphQLObjectType({
-  name: "RootQueryTypeeeeee", // this is the name that will appear in either GraphiQL or GraphQL Playground in the introspective self-documentation;
-  description: "This is hello, this is goodbye", // this is the description that will appear in either GraphiQL or GraphQL Playground in the introspective self-documentation;
+  name: "RootQueryType", // this is the name that will appear in either GraphiQL or GraphQL Playground in the introspective self-documentation;
+  description: "This is a description", // this is the description that will appear in either GraphiQL or GraphQL Playground in the introspective self-documentation;
   fields: {
     book: {
       type: BookType,
